594f4b5070db260c30e8dce18d2f625b37cc0de6
Damien <damien@damienvalade.fr>
2020-12-07 12:48:20 +0100

    exo heritage

---
+++ heritage.ts
@@ -0,0 +1,21 @@
+abstract class Music{
+    protected _instrument: string = 'nothing';
+    abstract makeSound(param): string;
+
+    get instrument():string{
+        return this._instrument;
+    }
+
+    play(): string{
+        return "play music";
+    }
+}
+
+class Guitare extends Music{
+    makeSound(param): string {
+        return param;
+    }
+}
+
+let guitare = new Guitare();
+console.log('I ' + guitare.play() + ' with my guitar : ' +guitare.makeSound('dring'));
